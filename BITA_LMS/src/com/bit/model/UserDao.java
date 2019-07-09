13bc8b405d9748c53edc7247faf5e806af538c82
Song Hae <thdgo456@naver.com>
2019-07-09 21:02:34 +0900

    Connection을 open 및 close 하는 Dao.class 생성, 각 0000Dao.java에 적용

--- BITA_LMS/src/com/bit/model/UserDao.java
+++ BITA_LMS/src/com/bit/model/UserDao.java
@@ -7,29 +7,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
-public class UserDao {
-	String driver = "oracle.jdbc.driver.OracleDriver";
-	String url = "jdbc:oracle:thin:@192.168.1.7:1521:xe";
-	//192.168.1.7
-	String user = "bita";
-	String password = "bita";
-	
-	Connection conn;
-	PreparedStatement pstmt;
-	ResultSet rs;
-	
-	public UserDao(){
-		try {
-			Class.forName(driver);
-			conn = DriverManager.getConnection(url,user,password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+public class UserDao extends Dao{
 	
 	public UserDto login(String id, String pw){
+		UserDto bean = new UserDto();
 		String sql = "SELECT u.user_Id AS user_id, password, u.name AS name, "
 				+ "email, phone_number, belong"
 				+ ", l.name AS lectureName, "
@@ -38,16 +19,16 @@ public class UserDao {
 				+ "JOIN lectureuser lu ON u.user_id = lu.user_id "
 				+ "JOIN lecture l ON lu.lecture_id = l.lecture_id "
 				+ "WHERE u.user_id=? AND u.password=?";
-		UserDto bean = new UserDto();
-		System.out.println(id+"$$"+pw);
-		System.out.println(sql);
+		
 		try {
+			openConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			pstmt.setString(2, pw);
 			rs = pstmt.executeQuery();
 			
 			if(rs.next()){
+				
 				bean.setUserId(rs.getString("user_id"));
 				bean.setPassword(rs.getString("password")); ///지우는게 나을까? 보안상 좋진 않은듯. 나중에 필요하지 않으면 지울것
 				bean.setName(rs.getString("name"));
@@ -57,18 +38,12 @@ public class UserDao {
 //				bean.setLectureName(rs.getString("lectureName"));
 //				bean.setStartDate(rs.getString("start_date"));
 //				bean.setEndDate(rs.getString("end_date"));
-//				bean.setLecture_id(rs.getInt("lecture_id"));
+				bean.setLectureId(rs.getInt("lecture_id"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally{
-			try {
-				if(rs!=null)rs.close();
-				if(pstmt!=null)pstmt.close();
-				if(conn!=null)conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			closeConnection();
 		}
 		return bean;
 	}
@@ -82,6 +57,7 @@ public class UserDao {
 				+ " values(?,?,?,?,?,?,'학생')";
 		
 		try {
+			openConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, bean.getUserId());
 			pstmt.setString(2, bean.getPassword());
@@ -94,12 +70,7 @@ public class UserDao {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally{
-			try {
-				if(pstmt!=null)pstmt.close();
-				if(conn!=null)conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			closeConnection();
 		}
 		return 0;
 	}
