039cf1b4a7c2224c0abd551feaa155456b0087fd
Song Hae <thdgo456@naver.com>
2019-07-15 19:16:52 +0900

    DB 제약조건 DELETE SET CASCADE로 변경

--- BITA_LMS/WebContent/WEB-INF/sql/bitaLmsDummy.sql
+++ BITA_LMS/WebContent/WEB-INF/sql/bitaLmsDummy.sql
@@ -1,4 +1,4 @@
-DELETE FROM USER01;
+﻿DELETE FROM USER01;
 DELETE FROM LECTURE;
 DELETE FROM LECTUREUSER;
 DELETE FROM TEACHER_INFO;
@@ -29,9 +29,9 @@ INSERT INTO user01(user_id,password,name,email,phone_number,inflow_path,belong) 
 
 INSERT INTO lecture(lecture_id,name,start_date,end_date,total_days,num_std,max_std,lv,content,is_close) values(-1,'행정',sysdate,sysdate,0,0,0,0,'content','0');
 INSERT INTO lecture(lecture_id,name,start_date,end_date,total_days,num_std,max_std,lv,content,is_close) values(0,'등록전',sysdate,sysdate,0,0,0,0,'content','0');
-INSERT INTO lecture(lecture_id,name,start_date,end_date,total_days,num_std,max_std,lv,content,is_close) values(1,'JAVA',sysdate,sysdate,60,0,30,3,'content','0');
-INSERT INTO lecture(lecture_id,name,start_date,end_date,total_days,num_std,max_std,lv,content,is_close) values(2,'DB',sysdate,sysdate,60,0,30,3,'content','0');
-INSERT INTO lecture(lecture_id,name,start_date,end_date,total_days,num_std,max_std,lv,content,is_close) values(3,'WEB',sysdate,sysdate,60,0,30,3,'content','0');
+INSERT INTO lecture(lecture_id,name,start_date,end_date,total_days,num_std,max_std,lv,content,is_close) values(1,'JAVA',TO_DATE('2019-06-01','YYYY-MM-DD'),TO_DATE('2019-08-30','YYYY-MM-DD'),60,0,30,3,'content','0');
+INSERT INTO lecture(lecture_id,name,start_date,end_date,total_days,num_std,max_std,lv,content,is_close) values(2,'DB',TO_DATE('2019-07-01','YYYY-MM-DD'),TO_DATE('2019-09-30','YYYY-MM-DD'),60,0,30,3,'content','0');
+INSERT INTO lecture(lecture_id,name,start_date,end_date,total_days,num_std,max_std,lv,content,is_close) values(3,'WEB',TO_DATE('2019-08-01','YYYY-MM-DD'),TO_DATE('2019-10-31','YYYY-MM-DD'),60,0,30,3,'content','0');
 
 --���� �Է��Ҷ� LectureUser ���̺��� �߰����ٰ� 
 INSERT INTO lectureUser(lecture_id, user_id) values(1,'tea1');
@@ -51,10 +51,6 @@ INSERT INTO lectureUser(lecture_id, user_id) values(0,'bef4');
 
 --학생의 강좌를 입력
 UPDATE 
-<<<<<<< HEAD
-
-=======
->>>>>>> song2
 
 
 --�л� ����� ���Ҷ� 
@@ -62,7 +58,7 @@ UPDATE
 --SELECT * FROM user01 u JOIN lectureUser l ON u.user_id=l.user_id;
 
 --Teacher_info
-INSERT INTO teacher_info(info_id,type ,content,teacher_id) VALUES(1,'자격증','정보처리기사','tea1');
+INSERT INTO teacher_info(info_id,type ,content,teacher_id) VALUES(info_id_seq.nextval,'자격증','정보처리기사','tea1');
 
 --Attendance
 INSERT INTO attendance(day_time,std_id,checkin_time,checkout_time,status,lecture_id) VALUES(TO_DATE('20190701','YYYYMMDD'),'stu1',TO_DATE('20190701 09:00:11','YYYYMMDD HH24:MI:SS') ,TO_DATE('20190701 18:00:11','YYYYMMDD HH24:MI:SS') ,'출석',1);
@@ -78,13 +74,13 @@ INSERT INTO score(lecture_id,std_id,first_score,second_score,third_score,avg_sco
 
 --Assignment
 
-INSERT INTO assignment(assignment_id, title, content, lecture_id, write_date) VALUES(1,'과제1','과제1 내용을 이러하다.',1,SYSDATE);
-INSERT INTO assignment(assignment_id, title, content, lecture_id, write_date) VALUES(2,'과제2','과제2 내용이다.',1,SYSDATE);
-INSERT INTO assignment(assignment_id, title, content, lecture_id, write_date) VALUES(3,'과제2','과제3 내용',1,SYSDATE);
+INSERT INTO assignment(assignment_id, title, content, lecture_id, write_date) VALUES(assignment_id_seq.NEXTVAL,'과제1','과제1 내용을 이러하다.',1,SYSDATE);
+INSERT INTO assignment(assignment_id, title, content, lecture_id, write_date) VALUES(assignment_id_seq.NEXTVAL,'과제2','과제2 내용이다.',1,SYSDATE);
+INSERT INTO assignment(assignment_id, title, content, lecture_id, write_date) VALUES(assignment_id_seq.NEXTVAL,'과제2','과제3 내용',1,SYSDATE);
 INSERT INTO assignment(assignment_id, title, content, lecture_id, write_date) VALUES(assignment_id_seq.NEXTVAL,'과제다!!','과제!! 내용!!',1,SYSDATE);
 
 --Submission
-INSERT INTO submission(assignment_id, std_id,submit_date,is_check) VALUES(1,'stu1',SYSDATE,'0');
+INSERT INTO submission(assignment_id, std_id,submit_date,is_check) VALUES(,'stu1',SYSDATE,'0');
 INSERT INTO submission(assignment_id, std_id,submit_date,is_check) VALUES(1,'stu2',SYSDATE,'0');
 INSERT INTO submission(assignment_id, std_id,submit_date,is_check) VALUES(1,'stu3',SYSDATE,'0');
 INSERT INTO submission(assignment_id, std_id,submit_date,is_check) VALUES(2,'stu2',SYSDATE,'0');
@@ -94,10 +90,10 @@ INSERT INTO submission(assignment_id, std_id,submit_date,is_check) VALUES(3,'stu
 
 --Qna_L
 --null 을 넣으면 안되니까 질문 생성시 빈칸으로 해서 생성할것
-INSERT INTO qna_l(qnal_id,std_id, type, title, question_content, responder_id, answer_content, write_date, is_check) VALUES(1,'stu1','의의신청','성적문의 입니다(제목)','성적이 잘못 나온것 같습니다. 수정 해주세요.','tea1','네 그게 맞습니다 더 공부하세요.',SYSDATE,1);
-INSERT INTO qna_l(qnal_id,std_id, type, title, question_content, responder_id, answer_content, write_date, is_check) VALUES(2,'stu2','성적문의','성적문의 입니다(제목)','성적이 잘못 나온것 같습니다. 수정 해주세요.','tea1','네 그게 맞습니다 더 공부하세요.',SYSDATE,0);
-INSERT INTO qna_l(qnal_id,std_id, type, title, question_content, responder_id, answer_content, write_date, is_check) VALUES(3,'stu3','강사','성적문의 입니다(제목)','성적이 잘못 나온것 같습니다. 수정 해주세요.','tea1','네 그게 맞습니다 더 공부하세요.',SYSDATE,0);
-INSERT INTO qna_l(qnal_id,std_id, type, title, question_content, responder_id, answer_content, write_date, is_check) VALUES(4,'stu1','행정','성적문의 입니다(제목)','성적이 잘못 나온것 같습니다. 수정 해주세요.','admin','네 그게 맞습니다 더 공부하세요.',SYSDATE,1);
+INSERT INTO qna_l(qnal_id,std_id, type, title, question_content, answer_content, write_date, is_check) VALUES(qnal_id_seq.nextval,'stu1','의의신청','성적문의 입니다(제목)','성적이 잘못 나온것 같습니다. 수정 해주세요.','네 그게 맞습니다 더 공부하세요.',SYSDATE,1);
+INSERT INTO qna_l(qnal_id,std_id, type, title, question_content, write_date, is_check) VALUES(qnal_id_seq.nextval,'stu2','성적문의','성적문의 입니다(제목)','성적이 잘못 나온것 같습니다. 수정 해주세요.',SYSDATE,0);
+INSERT INTO qna_l(qnal_id,std_id, type, title, question_content, write_date, is_check) VALUES(qnal_id_seq.nextval,'stu3','강사','성적문의 입니다(제목)','성적이 잘못 나온것 같습니다. 수정 해주세요.',SYSDATE,0);
+INSERT INTO qna_l(qnal_id,std_id, type, title, question_content, answer_content, write_date, is_check) VALUES(qnal_id_seq.nextval,'stu1','행정','공결 처리 문의 입니다.','~~~ 맞나요?','0000 입니다.',SYSDATE,1);
 
 --Calendar
 INSERT INTO calendar(calendar_id, title, content, start_date, end_date, lecture_id) VALUES(calendar_id_seq.NEXTVAL,'java 개강', '개강이다!', TO_DATE('2019-07-01 00:00:00','YYYY-MM-DD HH24:MI:SS'), TO_DATE('2019-07-01 23:59:00','YYYY-MM-DD HH24:MI:SS'),1);
@@ -107,14 +103,10 @@ INSERT INTO calendar(calendar_id, title, content, start_date, end_date, lecture_
 INSERT INTO calendar(calendar_id, title, content, start_date, end_date, lecture_id) VALUES(calendar_id_seq.NEXTVAL,'1차 시험', 'java, sql 시험 있습니다.', TO_DATE('2019-07-25 00:00:00','YYYY-MM-DD HH24:MI:SS'), TO_DATE('2019-07-26 23:59:00','YYYY-MM-DD HH24:MI:SS'),1);
 INSERT INTO calendar(calendar_id, title, content, start_date, end_date, lecture_id) VALUES(calendar_id_seq.NEXTVAL,'DB반 종강', '수고하셨습니다.', TO_DATE('2019-07-30 00:00:00','YYYY-MM-DD HH24:MI:SS'), TO_DATE('2019-07-30 23:59:00','YYYY-MM-DD HH24:MI:SS'),1);
 INSERT INTO calendar(calendar_id, title, content, start_date, end_date, lecture_id) VALUES(calendar_id_seq.NEXTVAL,'중복!', '삼계탕 먹으로 갑시다', TO_DATE('2019-07-27 00:00:00','YYYY-MM-DD HH24:MI:SS'), TO_DATE('2019-07-27 23:59:00','YYYY-MM-DD HH24:MI:SS'),1);
-<<<<<<< HEAD
  
-commit;
-=======
 
 --apply
 INSERT INTO apply(apply_date,apply_id,lecture_id,user_id,file_id) VALUES(sysdate,1,1,'bef1',1);
 INSERT INTO apply(apply_date,apply_id,lecture_id,user_id,file_id) VALUES(sysdate,2,1,'bef2',2);
 INSERT INTO apply(apply_date,apply_id,lecture_id,user_id,file_id) VALUES(sysdate,3,1,'bef3',3);
 commit;
->>>>>>> song2
